5d8fb567151c780ace36c6e512f92436e3124a06
jslimt <158132238+jslimt@users.noreply.github.com>
2024-02-06 11:04:41 -0700

    Initial commit

---
+++ Labs/Lab 3/lab.py
@@ -0,0 +1,158 @@
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+
+
+# import libraries
+import numpy as np
+
+def driver():
+
+# use routines    
+    f = lambda x: x**2*(x-1)
+    a = -1
+    b = 2
+    
+    
+
+#    f = lambda x: np.sin(x)
+#    a = 0.1
+#    b = np.pi+0.1
+
+    tol = 1e-7
+
+    [astar,ier] = bisection(f,a,b,tol)
+    #print('f = x^2 * (x-1) E (-1,.5)')
+    print('the approximate root is',astar)
+    print('the error message reads:',ier)
+    print('f(astar) =', f(astar))
+
+
+
+
+# define routines
+def bisection(f,a,b,tol):
+    
+#    Inputs:
+#     f,a,b       - function and endpoints of initial interval
+#      tol  - bisection stops when interval length < tol
+
+#    Returns:
+#      astar - approximation of root
+#      ier   - error message
+#            - ier = 1 => Failed
+#            - ier = 0 == success
+
+#     first verify there is a root we can find in the interval 
+
+    fa = f(a)
+    fb = f(b);
+    if (fa*fb>0):
+       ier = 1
+       astar = a
+       return [astar, ier]
+
+#   verify end points are not a root 
+    if (fa == 0):
+      astar = a
+      ier =0
+      return [astar, ier]
+
+    if (fb ==0):
+      astar = b
+      ier = 0
+      return [astar, ier]
+
+    count = 0
+    d = 0.5*(a+b)
+    while (abs(d-a)> tol):
+      fd = f(d)
+      if (fd ==0):
+        astar = d
+        ier = 0
+        return [astar, ier]
+      if (fa*fd<0):
+         b = d
+      else: 
+        a = d
+        fa = fd
+      d = 0.5*(a+b)
+      count = count +1
+#      print('abs(d-a) = ', abs(d-a))
+      
+    astar = d
+    ier = 0
+    return [astar, ier]
+      
+ 
+       
+
+
+# import libraries
+import numpy as np
+    
+def drivera():
+
+# test functions 
+     f1 = lambda x: 1+0.5*np.sin(x)
+# fixed point is alpha1 = 1.4987....
+
+     f2 = lambda x: 3+2*np.sin(x)
+#fixed point is alpha2 = 3.09... 
+
+     Nmax = 100
+     tol = 1e-6
+
+# test f1 '''
+     x0 = 0.0
+     [xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+     print('the approximate fixed point is:',xstar)
+     print('f1(xstar):',f1(xstar))
+     print('Error message reads:',ier)
+    
+#test f2 '''
+     x0 = 0.0
+     [xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+     print('the approximate fixed point is:',xstar)
+     print('f2(xstar):',f2(xstar))
+     print('Error message reads:',ier)
+
+
+
+# define routines
+def fixedpt(f,x0,tol,Nmax):
+
+    ''' x0 = initial guess''' 
+    ''' Nmax = max number of iterations'''
+    ''' tol = stopping tolerance'''
+
+    count = 0
+    while (count <Nmax):
+       count = count +1
+       x1 = f(x0)
+       if (abs(x1-x0) <tol):
+          xstar = x1
+          ier = 0
+          return [xstar,ier]
+       x0 = x1
+
+    xstar = x1
+    ier = 1
+    return [xstar, ier]
+    
+
+f1A = lambda: x**2*(x-1)
+a1a = 1/2
+b1b = 2
+
+
+driver()
+
+
